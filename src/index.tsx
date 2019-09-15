66299221175e35d697cc48e40c32050e605d3de4
SoraKumo <info@croud.jp>
2019-09-15 14:02:58 +0900

    サンプルを選択式に変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,54 @@
-import * as React from "react";
+import React, { ReactElement } from "react";
 import * as ReactDOM from "react-dom";
-import { JSWindow, TreeView, TreeItem, SplitView } from "@jswf/react";
+import { JSWindow, ListView } from "@jswf/react";
+import { SimpleWindow } from "./Samples/SimpleWindow";
+import { BasicWindow } from "./Samples/BasicWindow";
+import { SplitBar } from "./Samples/SplitBar";
+import { ListViewTest } from "./Samples/ListViewTest";
+import { TreeViewTest } from "./Samples/TreeViewTest";
 
-function App() {
-  const [message,setMessage] = React.useState();
-  const treeView = React.useRef<TreeView>(null);
+let key = 0;
+const Compornents: [() => JSX.Element, string][] = [
+  [SimpleWindow, "Simple Window usage"],
+  [BasicWindow, "Basic Parameter Window usage"],
+  [SplitBar, "SplitBar usage"],
+  [ListViewTest, "ListView usage"],
+  [TreeViewTest, "TreeView usage"]
+];
+function CompornentList() {
+  let count = 0;
+  const [nodes, setNodes] = React.useState<ReactElement[]>([]);
   return (
     <>
-      <JSWindow width={600} title="TreeViewの開発中">
-        <SplitView>
+      <JSWindow x={0} y={0} width={600} title="Sample list">
+        <ListView onItemClick={onItemClick}>
           <div>
-            <button onClick={()=>{
-              const item = treeView.current!.getSelectItem();
-              if(item)
-                item.addItem({label:"追加"});
-            }}>追加</button><br/><br/>
-            <button onClick={()=>{
-              const item = treeView.current!.getSelectItem();
-                if(item)
-                  item.remove();
-              }}>選択を削除</button><br/><br/>
-            <button onClick={()=>{
-              const items = treeView.current!.getCheckItems();
-              for(const item of items)
-                  item.remove();
-              }}>チェックを削除</button><br/><br/>
-            {message}
+            <div data-type="number">No</div>
+            <div data-width={200}>Name</div>
+            <div>Info</div>
           </div>
-
-          <TreeView ref={treeView} onItemClick={(item)=>setMessage(item.getLabel())}>
-            <TreeItem label="Root">
-              <TreeItem label="Data2">
-                <TreeItem label="Data3"><TreeItem label="Data4"></TreeItem></TreeItem>
-              </TreeItem>
-              <TreeItem label={<><b>太字</b></>}>
-                <TreeItem label={<>改行を<br/>含む</>}></TreeItem>
-              </TreeItem>
-              <TreeItem label={<><input defaultValue="TextBoxも入る"/></>}/>
-            </TreeItem>
-          </TreeView>
-        </SplitView>
+          {Compornents.map((c, index) => (
+            <div key={index}>
+              <div>{++count}</div>
+              <div>{c[0].name}</div>
+              <div>{c[1]}</div>
+            </div>
+          ))}
+        </ListView>
       </JSWindow>
+      {nodes}
     </>
   );
+  function onItemClick(row: number, col: number) {
+    const newNodes = nodes.filter(node => {
+      return node.type !== Compornents[row][0];
+    });
+    newNodes.push(React.createElement(Compornents[row][0], { key: key++ }));
+    setNodes(newNodes.slice());
+  }
 }
 
+function App() {
+  return <CompornentList />;
+}
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
